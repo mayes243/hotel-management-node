36f8eda7c58e59140a0dd1691cd910e50a72c418
mayeskhatri243 <mayeskhatri243@gmail.com>
2024-01-05 14:50:14 +0530

    COM: cookie and passport issue solved

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,229 +1,216 @@
-const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
-const bcrypt = require('bcryptjs');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
 // Local Imports
-const countries = require('./../utils/countriesData');
-const fileUpload = require('./../utils/fileUpload');
-const { deleteImage } = require('./../utils/customFunctions');
+const countries = require("./../utils/countriesData");
+const fileUpload = require("./../utils/fileUpload");
+const { deleteImage } = require("./../utils/customFunctions");
 
-const User = require('./../models/User');
-const Dish = require('./../models/Dish');
-const Order = require('./../models/Order');
+const User = require("./../models/User");
+const Dish = require("./../models/Dish");
+const Order = require("./../models/Order");
 
 const signToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 };
 
 const jwtToken = (user, res) => {
+  const token = signToken(user._id);
 
-    const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV !== "development",
+  };
 
-    const cookieOptions = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        // secure: true,
-        httpOnly: true
-    };
-
-    res.cookie('jwt', token, cookieOptions);
-    res.redirect('/');
+  res.cookie("jwt", token, cookieOptions);
+  res.redirect("/");
 };
 
 exports.submitRegister = async (req, res) => {
-    const newUser = new User({
-        ...req.body
-    });
+  const newUser = new User({
+    ...req.body,
+  });
 
-    const user = await newUser.save();
+  const user = await newUser.save();
 
-    jwtToken(user, res);
+  jwtToken(user, res);
 };
 
 exports.submitLogin = async (req, res) => {
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
+  const user = await User.findOne({ email: email }).select("+password");
 
-    const user = await User.findOne({ email: email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    req.flash("error-message", "Incorrect Email or Password");
+    return res.redirect("/login");
+  }
 
-    if (!user || !(await user.correctPassword(password, user.password))) {
-        req.flash('error-message', 'Incorrect Email or Password');
-        return res.redirect('/login');
-    };
-
-    jwtToken(user, res);
+  jwtToken(user, res);
 };
 
 exports.protect = async (req, res, next) => {
+  // 1) Getting Token and check if it's therefore
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
 
-    // 1) Getting Token and check if it's therefore
-    let token
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(' ')[1];
-    } else if (req.cookies.jwt) {
-        token = req.cookies.jwt;
-    }
+  if (!token) {
+    req.flash("error-message", "Please Login!");
+    return res.redirect("/login");
+  }
 
-    if (!token) {
-        req.flash('error-message', 'Please Login!');
-        return res.redirect('/login');
-    }
+  // 2) Verification Token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-    // 2) Verification Token
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
 
-    // 3) Check if user still exists
-    const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    req.flash("error-message", "Please Login!");
+    return res.redirect("/login");
+  }
 
-    if (!currentUser) {
-        req.flash('error-message', 'Please Login!');
-        return res.redirect('/login');
-    }
-
-    // Grant Access to Protected Route
-    req.user = currentUser;
-    res.locals.user = currentUser;
-    next();
+  // Grant Access to Protected Route
+  req.user = currentUser;
+  res.locals.user = currentUser;
+  next();
 };
 
 exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) Verify Token
+      const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
 
-    if (req.cookies.jwt) {
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
 
-        try {
+      if (!currentUser) {
+        return next();
+      }
 
-            // 1) Verify Token
-            const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
-
-            // 2) Check if user still exists
-            const currentUser = await User.findById(decoded.id);
-
-            if (!currentUser) {
-                return next();
-            }
-
-            // There is a logged in user
-            req.client = currentUser;
-            res.locals.user = currentUser;
-            return next();
-
-        } catch (err) {
-            return next();
-        }
-
+      // There is a logged in user
+      req.client = currentUser;
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
     }
+  }
 
-    next();
+  next();
 };
 
 exports.userLogout = (req, res) => {
-    res.cookie('jwt', 'loggedout', {
-        expires: new Date(Date.now() + 10 * 1000),
-        httpOnly: true
-    });
+  res.cookie("jwt", "loggedout", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
 
-    res.redirect('/login');
+  res.redirect("/login");
 };
 
 exports.getDashboard = async (req, res) => {
+  const userId = req.client._id;
+  const userOrders = await Order.find({ user: userId }).populate("dish");
 
-    const userId = req.client._id;
-    const userOrders = await Order.find({ user: userId }).populate('dish');
-
-    res.render('frontend/user/dashboard', {
-        title: 'User Dashboard',
-        userOrders
-    });
+  res.render("frontend/user/dashboard", {
+    title: "User Dashboard",
+    userOrders,
+  });
 };
 
 // Order
 
 exports.submitOrder = async (req, res) => {
-    const orderedItem = await Dish.findById(req.params.dish);
-    const user = await User.findById(req.params.user);
+  const orderedItem = await Dish.findById(req.params.dish);
+  const user = await User.findById(req.params.user);
 
-    const newOrder = new Order({
-        dish: orderedItem._id,
-        user: user._id
-    });
+  const newOrder = new Order({
+    dish: orderedItem._id,
+    user: user._id,
+  });
 
-    const savedOrder = await newOrder.save();
-    req.flash('success-message', 'Order successfully submitted.');
-    res.redirect('/user/dashboard');
+  const savedOrder = await newOrder.save();
+  req.flash("success-message", "Order successfully submitted.");
+  res.redirect("/user/dashboard");
 };
 
 exports.deleteOrder = async (req, res) => {
-    const orderId = req.params.id;
+  const orderId = req.params.id;
 
-    const deletedOrder = await Order.findByIdAndDelete(orderId).populate('dish');
-    req.flash('success-message', `${deletedOrder.dish.name} has been Cancelled!`);
-    res.redirect('/user/dashboard');
-}
+  const deletedOrder = await Order.findByIdAndDelete(orderId).populate("dish");
+  req.flash("success-message", `${deletedOrder.dish.name} has been Cancelled!`);
+  res.redirect("/user/dashboard");
+};
 
 // User Profile
 
-exports.uploadImage = fileUpload.single('image');
+exports.uploadImage = fileUpload.single("image");
 
 exports.getUserProfile = (req, res) => {
-
-    res.render('frontend/user/profile', {
-        title: 'Edit Profile',
-        countries
-    });
+  res.render("frontend/user/profile", {
+    title: "Edit Profile",
+    countries,
+  });
 };
 
 exports.submitUserProfile = async (req, res) => {
+  const user = req.user;
 
-    const user = req.user;
+  let image = user.image;
 
-    let image = user.image;
+  if (req.file) {
+    deleteImage(user, req);
+    image = req.file.filename;
+  }
 
-    if (req.file) {
-        deleteImage(user, req);
-        image = req.file.filename;
-    }
+  const updatedUser = await User.findByIdAndUpdate(user._id, {
+    ...req.body,
+    image,
+  });
 
-    const updatedUser = await User.findByIdAndUpdate(user._id, {
-        ...req.body,
-        image
-    });
-
-    req.flash('success-message', `Profile Successfully Updated!`);
-    res.redirect(req.originalUrl);
+  req.flash("success-message", `Profile Successfully Updated!`);
+  res.redirect(req.originalUrl);
 };
 
 exports.submitChangePassword = async (req, res) => {
+  const id = req.user._id;
 
-    const id = req.user._id;
+  const user = await User.findById(id).select("+password");
 
-    const user = await User.findById(id).select('+password');
+  const { oldpassword, newpassword } = req.body;
 
-    const { oldpassword, newpassword } = req.body;
+  const passwordMatched = await user.correctPassword(oldpassword, user.password);
 
-    const passwordMatched = await user.correctPassword(oldpassword, user.password);
+  if (!passwordMatched) {
+    req.flash("error-message", "Please enter correct password.");
+    return res.redirect("/user/profile");
+  }
 
-    if (!passwordMatched) {
-        req.flash('error-message', 'Please enter correct password.')
-        return res.redirect('/user/profile');
-    };
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(newpassword, salt);
 
-    const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(newpassword, salt);
+  const updatedUser = await User.findByIdAndUpdate(user._id, {
+    password,
+  });
 
-    const updatedUser = await User.findByIdAndUpdate(user._id, {
-        password
-    });
-
-    req.flash('success-message', 'Password Successfully Updated!');
-    res.redirect('/user/profile');
-
+  req.flash("success-message", "Password Successfully Updated!");
+  res.redirect("/user/profile");
 };
 
 exports.getUserRecommed = (req, res) => {
-
-    res.render('frontend/user/recommed', {
-        title: 'Recommed',
-    });
+  res.render("frontend/user/recommed", {
+    title: "Recommed",
+  });
 };
--- routes/backendRoutes.js
+++ routes/backendRoutes.js
@@ -1,129 +1,143 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 
 // Local Imports
-const authControllers = require('./../controllers/authControllers');
-const backendControllers = require('./../controllers/backendControllers');
-const dishesControllers = require('./../controllers/dishesControllers');
-const categoryControllers = require('./../controllers/categoryControllers');
-const testimonialControllers = require('./../controllers/testimonialControllers');
-const bannerControllers = require('./../controllers/bannerControllers');
-const galleryControllers = require('./../controllers/galleryControllers');
+const authControllers = require("./../controllers/authControllers");
+const backendControllers = require("./../controllers/backendControllers");
+const dishesControllers = require("./../controllers/dishesControllers");
+const categoryControllers = require("./../controllers/categoryControllers");
+const testimonialControllers = require("./../controllers/testimonialControllers");
+const bannerControllers = require("./../controllers/bannerControllers");
+const galleryControllers = require("./../controllers/galleryControllers");
 
-const { initializeAdmin } = require('./../passport-config');
+const { initializeAdmin } = require("./../passport-config");
 initializeAdmin(passport);
 
 // Create Router
 const router = express.Router();
 
+const auth = passport.authenticate("local", {
+  successRedirect: "/admin/dashboard",
+  failureRedirect: "/admin/login",
+  failureFlash: true,
+  successFlash: true,
+  session: true,
+});
+
 // Authentication
-router.route('/login')
-    .get(backendControllers.getLogin)
-    .post(passport.authenticate('local', {
-        successRedirect: '/admin/dashboard',
-        failureRedirect: '/admin/login',
-        failureFlash: true,
-        successFlash: true,
-        session: true
-    }));
+router.route("/login").get(backendControllers.getLogin).post(auth);
 
 router.use(authControllers.adminAuth);
 
 router.use(backendControllers.globalAdminVariables);
 
-router.route('/logout')
-    .get(authControllers.adminLogout);
+router.route("/logout").get(authControllers.adminLogout);
 
 // Basic Dashboard
-router.get('/dashboard', backendControllers.getDashboard);
+router.get("/dashboard", backendControllers.getDashboard);
 
 // Admin Profile
 
-router.route('/profile')
-    .get(backendControllers.getProfileSettings);
+router.route("/profile").get(backendControllers.getProfileSettings);
 
-router.put('/profile/:id', backendControllers.uploadSingle, backendControllers.submitProfileSettings);
+router.put(
+  "/profile/:id",
+  backendControllers.uploadSingle,
+  backendControllers.submitProfileSettings
+);
 
 // Site Settings
-router.get('/site-settings', backendControllers.getSiteSettings);
-router.put('/site-settings/:id', backendControllers.uploadSiteSettings, backendControllers.submitSiteSettings);
+router.get("/site-settings", backendControllers.getSiteSettings);
+router.put(
+  "/site-settings/:id",
+  backendControllers.uploadSiteSettings,
+  backendControllers.submitSiteSettings
+);
 
 // Dishes
-router.get('/all-dishes', dishesControllers.getAllDishes);
+router.get("/all-dishes", dishesControllers.getAllDishes);
 
-router.route('/add-dish')
-    .get(dishesControllers.getAddDish)
-    .post(dishesControllers.uploadImage, dishesControllers.submitAddDish);
+router
+  .route("/add-dish")
+  .get(dishesControllers.getAddDish)
+  .post(dishesControllers.uploadImage, dishesControllers.submitAddDish);
 
-router.route('/edit-dish/:id')
-    .get(dishesControllers.getEditDish)
-    .put(dishesControllers.uploadImage, dishesControllers.submitEditDish);
+router
+  .route("/edit-dish/:id")
+  .get(dishesControllers.getEditDish)
+  .put(dishesControllers.uploadImage, dishesControllers.submitEditDish);
 
-router.delete('/dish/delete/:id', dishesControllers.deleteDish);
+router.delete("/dish/delete/:id", dishesControllers.deleteDish);
 
-router.route('/special')
-    .get(dishesControllers.getSpecialDishes)
-    .post(dishesControllers.submitSpecialDish);
+router
+  .route("/special")
+  .get(dishesControllers.getSpecialDishes)
+  .post(dishesControllers.submitSpecialDish);
 
-router.delete('/special/:id', dishesControllers.deleteSpecial);
+router.delete("/special/:id", dishesControllers.deleteSpecial);
 
 // Category
 
-router.route('/category')
-    .get(categoryControllers.getAllCategory)
-    .post(categoryControllers.submitCategory);
+router
+  .route("/category")
+  .get(categoryControllers.getAllCategory)
+  .post(categoryControllers.submitCategory);
 
-router.route('/category/:id')
-    .get(categoryControllers.getEditCategory)
-    .put(categoryControllers.submitEditCategory)
-    .delete(categoryControllers.deleteCategory);
+router
+  .route("/category/:id")
+  .get(categoryControllers.getEditCategory)
+  .put(categoryControllers.submitEditCategory)
+  .delete(categoryControllers.deleteCategory);
 
 // Testimonial
 
-router.get('/all-testimonials', testimonialControllers.getAllTestimonials);
+router.get("/all-testimonials", testimonialControllers.getAllTestimonials);
 
-router.route('/add-testimonial')
-    .get(testimonialControllers.getAddTestimonial)
-    .post(testimonialControllers.uploadImage, testimonialControllers.submitAddTestimonial);
+router
+  .route("/add-testimonial")
+  .get(testimonialControllers.getAddTestimonial)
+  .post(testimonialControllers.uploadImage, testimonialControllers.submitAddTestimonial);
 
-router.route('/testimonial/:id')
-    .get(testimonialControllers.getEditTestimonial)
-    .put(testimonialControllers.uploadImage, testimonialControllers.submitEditTestimonial)
-    .delete(testimonialControllers.deleteTestimonial);
+router
+  .route("/testimonial/:id")
+  .get(testimonialControllers.getEditTestimonial)
+  .put(testimonialControllers.uploadImage, testimonialControllers.submitEditTestimonial)
+  .delete(testimonialControllers.deleteTestimonial);
 
 // Banner
 
-router.get('/all-banners', bannerControllers.getAllBanners);
+router.get("/all-banners", bannerControllers.getAllBanners);
 
-router.route('/add-banner')
-    .get(bannerControllers.getAddBanner)
-    .post(bannerControllers.uploadImage, bannerControllers.submitAddBanner);
+router
+  .route("/add-banner")
+  .get(bannerControllers.getAddBanner)
+  .post(bannerControllers.uploadImage, bannerControllers.submitAddBanner);
 
-router.route('/banner/:id')
-    .get(bannerControllers.getEditBanner)
-    .put(bannerControllers.uploadImage, bannerControllers.submitEditBanner)
-    .delete(bannerControllers.deleteBanner);
+router
+  .route("/banner/:id")
+  .get(bannerControllers.getEditBanner)
+  .put(bannerControllers.uploadImage, bannerControllers.submitEditBanner)
+  .delete(bannerControllers.deleteBanner);
 
 // Gallery
 
-router.route('/gallery')
-    .get(galleryControllers.getGallery);
+router.route("/gallery").get(galleryControllers.getGallery);
 
-router.route('/add-image')
-    .get(galleryControllers.getAddImage)
-    .post(galleryControllers.uploadImage, galleryControllers.submitAddImage);
+router
+  .route("/add-image")
+  .get(galleryControllers.getAddImage)
+  .post(galleryControllers.uploadImage, galleryControllers.submitAddImage);
 
-router.delete('/image/:id', galleryControllers.deleteImage);
+router.delete("/image/:id", galleryControllers.deleteImage);
 
 // Messages
 
-router.get('/messages', backendControllers.getAllMessages);
+router.get("/messages", backendControllers.getAllMessages);
 
-router.delete('/message/:id', backendControllers.deleteMessage);
+router.delete("/message/:id", backendControllers.deleteMessage);
 
 // order aceept
-router.post('/:id', backendControllers.updateOrder);
-router.get('/accept/order/:id', backendControllers.acceptOrder);
-
+router.post("/:id", backendControllers.updateOrder);
+router.get("/accept/order/:id", backendControllers.acceptOrder);
 
 module.exports = router;
