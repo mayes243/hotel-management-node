5c8bf7ab1cc0ce5db7fc27f7d637fc40413e51ab
mayeskhatri243 <mayeskhatri243@gmail.com>
2024-01-21 17:05:19 +0530

    Helmet configured

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Feature-Policy", 'geolocation "self"');
+  res.setHeader("Permissions-Policy", 'geolocation=(self "https://example.com"), microphone=()');
   next();
 });
 
