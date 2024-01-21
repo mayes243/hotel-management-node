d7148e32da86bc6c2a61f6085cc33cc18eb988bd
mayeskhatri243 <mayeskhatri243@gmail.com>
2024-01-21 16:34:56 +0530

    Helmet configured

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
         styleSrc: ["'self'", "'unsafe-inline'"],
         imgSrc: ["'self'", "data:"],
       },
